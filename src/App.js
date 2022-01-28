abdebeac14c658bdc6129d5500c2a3ee3fb90ac1
N00b13-M1 <michael.cornelis@gmail.com>
2022-01-28 12:48:01 +0100

    ImgVisibleNow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React, {Component} from 'react';
 
-class App extends Component {
+// # Front-end Style Guide
+// ## Layou
+// The designs were created to the following widths:
+
+// - Mobile: 375px
+// - Desktop: 1440px
+// ## Colors
+// ### Primary
+// - Soft blue: hsl(215, 51%, 70%)
+// - Cyan: hsl(178, 100%, 50%)
+// ### Neutral
+
+// - Very dark blue (main BG): hsl(217, 54%, 11%)
+// - Very dark blue (card BG): hsl(216, 50%, 16%)
+// - Very dark blue (line): hsl(215, 32%, 27%)
+// - White: hsl(0, 0%, 100%)
+// ## Typography
+// ### Body Copy
+// - Font size (paragraph): 18px
+// ### Font
+
+// -Family: [Outfit](https://fonts.google.com/specimen/Outfit)
+// - Weights: 300, 400, 600
+
+
+import React from 'react';
+import Card from './components/card/Card';
+
+class App extends React.Component {
   render(){
     return (
-      <div>
-      Hello World
+      <div className="bg-primary">
+        <Card/>
       </div>
     );
   }
